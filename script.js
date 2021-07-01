7a10ccfa97f0eeda13b2841b6e7c414376457589
Tabata Nobuya <59525219+ponta1106@users.noreply.github.com>
2021-07-01 20:20:13 +0900

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-btn = document.getElementById('btn');
-
-btn.addEventListener('click', ()=> {
-  textarea = document.getElementById('textarea');
-  const content = textarea.value;
-  alert(content);
-})
